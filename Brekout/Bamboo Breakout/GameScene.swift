1fb8e320e99173b401e007bd770d7492bf990b94
Shivani Dhanasamy <sdhanas1@slic-39.pods.bu.int>
2019-02-27 20:59:24 -0500

    paddle

--- Brekout/Bamboo Breakout/GameScene.swift
+++ Brekout/Bamboo Breakout/GameScene.swift
@@ -29,9 +29,14 @@ let BallCategoryName = "ball"
 let PaddleCategoryName = "paddle"
 let BlockCategoryName = "block"
 let GameMessageName = "gameMessage"
+let BallCategory   : UInt32 = 0x1 << 0
+let BottomCategory : UInt32 = 0x1 << 1
+let BlockCategory  : UInt32 = 0x1 << 2
+let PaddleCategory : UInt32 = 0x1 << 3
+let BorderCategory : UInt32 = 0x1 << 4
 
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     var isFingerOnPaddle = false
   
   override func didMove(to view: SKView) {
@@ -39,6 +44,21 @@ class GameScene: SKScene {
     let borderBody = SKPhysicsBody(edgeLoopFrom: self.frame)
     borderBody.friction = 0
     self.physicsBody = borderBody
+    
+    physicsWorld.contactDelegate = self
+
+  
+    let bottomRect = CGRect(x: frame.origin.x, y: frame.origin.y, width: frame.size.width, height: 1)
+    let bottom = SKNode()
+    bottom.physicsBody = SKPhysicsBody(edgeLoopFrom: bottomRect)
+    addChild(bottom)
+    let paddle = childNode(withName: PaddleCategoryName) as! SKSpriteNode
+    
+    bottom.physicsBody!.categoryBitMask = BottomCategory
+   
+    paddle.physicsBody!.categoryBitMask = PaddleCategory
+    borderBody.categoryBitMask = BorderCategory
+    
     
  }
  
@@ -75,6 +95,24 @@ class GameScene: SKScene {
   
  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         isFingerOnPaddle = false
+    }
+    
+ func didBegin(_ contact: SKPhysicsContact) {
+        // 1
+        var firstBody: SKPhysicsBody
+        var secondBody: SKPhysicsBody
+        // 2
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
+        // 3
+        if firstBody.categoryBitMask == BallCategory && secondBody.categoryBitMask == BottomCategory {
+            print("Hit bottom. First contact has been made.")
+        }
     }
   
 }
